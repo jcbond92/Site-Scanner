7368736d49897d240cd25fe98dedd5033ff09534
Joseph Bond <jcbond92@gmail.com>
2021-10-19 13:52:56 -0400

    fix: remove old example files

--- examples/link-checker.mjs
+++
@@ -1,8 +0,0 @@
-import statusChecker from "../src/link-checker/app.js";
-
-const config = {
-  url: "https://www.truist.com/wealth.index.xml",
-  path: ".examples/link-checker-results.json",
-};
-
-statusChecker(config);
