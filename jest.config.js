8544611655bcc43ed267ab2d09fc0dcb42789fe6
Joseph Bond <jcbond92@gmail.com>
2021-10-19 23:16:17 -0400

    fix: all tests passing

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
   preset: "ts-jest/presets/js-with-babel",
-  transform: { "^.+\\.(ts|tsx)$": "ts-jest" },
+  transform: { "^.+\\.(ts|js)$": "ts-jest" },
   testEnvironment: "node",
   moduleFileExtensions: ["ts", "js"],
   transformIgnorePatterns: ["node_modules/(?!@mymodule)"],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.js?$": "babel-jest",
   },
 };
--- src/link-checker/tests/app.spec.ts
+++ src/link-checker/tests/app.spec.ts
@@ -1,21 +1,22 @@
 import statusChecker from "../app";
+describe("error checking the config", () => {
+  test("no url included", () => {
+    const config = {
+      url: "",
+      path: "./examples/link-checker-results.json",
+      test: true
+    };
 
-test("no url included", () => {
-  const config = {
-    url: "",
-    path: "./examples/link-checker-results.json",
-    test: true
-  };
+    expect(statusChecker(config)).toBe(false);
+  });
 
-  expect(statusChecker(config)).toBe(false);
-});
+  test("no path included", () => {
+    const config = {
+      url: "https://www.google.com",
+      path: "",
+      test: true
+    };
 
-test("no path included", () => {
-  const config = {
-    url: "https://www.google.com",
-    path: "",
-    test: true
-  };
-
-  expect(statusChecker(config)).toBe(false);
+    expect(statusChecker(config)).toBe(false);
+  });
 });
